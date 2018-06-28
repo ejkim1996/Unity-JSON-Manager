d8b3ab57cf6c74e04df95a11177117914daef7c9
jwanggggg <30448951+jwanggggg@users.noreply.github.com>
2018-06-28 16:45:14 -0400

    Added Translatable script
    Currently contains dropdown menu/field for string

---
+++ Translatable.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class Translatable : MonoBehaviour
+{
+    public string id;
+    public options JSONFile;
+
+    public enum options
+    {
+        Option1,
+        Option2,
+        Option3
+    };
+
+}
+
