a1d4225f746216043754f97481d27ea86a1835e8
jwanggggg <30448951+jwanggggg@users.noreply.github.com>
2018-06-28 15:09:46 -0400

    Added JSON Editor Script, Script, and Prefab

---
+++ JSONScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using UnityEngine;
+using UnityEngine.UI;
+[System.Serializable]
+public class JSONScript : MonoBehaviour {
+
+    public string fileName;
+	
+    public void BuildJSON(string newFileName) {
+        string filePath = Path.Combine(Application.streamingAssetsPath, newFileName);
+        // Create a new file
+        if (!File.Exists(filePath)) {
+            file myFile = new file();
+            myFile.fileName = newFileName;
+
+            string json = JsonUtility.ToJson(myFile);
+
+            File.WriteAllText(filePath, json);
+
+            Debug.Log("Success");
+        } else {
+            Debug.LogError("File with this name already exists!");
+        }
+
+
+    }
+
+}
+
+// File class with datafields
+
+[System.Serializable]
+public class file {
+    public string fileName;
+}
---
+++ JSONScriptEditor.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(JSONScript))]
+public class JSONScriptEditor : Editor {
+
+	public override void OnInspectorGUI()
+	{
+        JSONScript myTarget = (JSONScript)target;
+
+        myTarget.fileName = EditorGUILayout.TextField("File Name", myTarget.fileName);
+
+        if(GUILayout.Button("Build JSON File")) {
+            myTarget.BuildJSON(myTarget.fileName);
+        }
+
+	}
+}
