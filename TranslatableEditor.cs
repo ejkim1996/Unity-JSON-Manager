f6a44161513b83ae8a967978614b6f4f910724a4
jwanggggg <30448951+jwanggggg@users.noreply.github.com>
2018-07-05 15:01:39 -0400

    Update TranslatableEditor.cs

--- TranslatableEditor.cs
+++ TranslatableEditor.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEditor;
 
 [CustomEditor(typeof(Translatable))]
@@ -11,17 +12,21 @@ public class TranslatableEditor : Editor
     public override void OnInspectorGUI()
     {
         Translatable myTarget = (Translatable)target;
+        // Call files
         myTarget.id = EditorGUILayout.TextField("ID", myTarget.id);
-        myTarget.selected = EditorGUILayout.Popup(myTarget.selected, myTarget.getFiles());
 
-        if (GUILayout.Button("Update with Text Component"))
+        myTarget.getFiles();
+        myTarget.selected = EditorGUILayout.Popup(myTarget.selected, myTarget.options);
+
+        if (GUILayout.Button("Update JSON File with Text Component"))
         {
-            DirectoryInfo directory = new DirectoryInfo(Application.streamingAssetsPath);
-            FileInfo[] info = directory.GetFiles();
-            foreach (FileInfo f in info)
-            {
-                Debug.Log(Path.GetFileName(f.ToString()));
-            }
+            // Finds the text component of the Game Object and corresponding file
+            string fileNameComponent = myTarget.id;
+            string textComponent = GameObject.Find("SampleGameObject").GetComponent<Text>().text;
+            string selectedFile = myTarget.options[myTarget.selected];
+
+            myTarget.UpdateJSON(selectedFile, fileNameComponent, textComponent);
+            Debug.Log("File was updated with text component.");
         }
 
     }
