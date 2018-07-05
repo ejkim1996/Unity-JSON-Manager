93fbffc7217d066419cf99c6682e4cdab0e8a043
jwanggggg <30448951+jwanggggg@users.noreply.github.com>
2018-07-05 15:02:26 -0400

    Translatable.cs can now update a JSON file

--- Translatable.cs
+++ Translatable.cs
@@ -3,14 +3,16 @@ using System.Collections.Generic;
 using System.IO;
 using UnityEngine;
 using UnityEditor;
+using UnityEngine.UI;
 
 public class Translatable : MonoBehaviour
 {
     public string id;
     public int selected;
     public string[] options;
+    public Text textObject;
 
-    public string[] getFiles() {
+    public void getFiles() {
         DirectoryInfo directory = new DirectoryInfo(Application.streamingAssetsPath);
         FileInfo[] info = directory.GetFiles();
         string[] fileNames = new string[info.Length];
@@ -18,8 +20,17 @@ public class Translatable : MonoBehaviour
         for (int i = 0; i < info.Length; i++) {
             fileNames[i] = Path.GetFileName(info[i].ToString());
         }
+        options = fileNames;
+    }
 
-        return fileNames;
+    public void UpdateJSON(string updateFileName, string fileNameComponent, string textComponent)
+    {
+        file updateFile = new file();
+        updateFile.fileName = fileNameComponent;
+        updateFile.text = textComponent;
+        string filePath = Path.Combine(Application.streamingAssetsPath, updateFileName);
+        string json = JsonUtility.ToJson(updateFile);
+        File.WriteAllText(filePath, json);
     }
 
 }
